b003e1d6945a6ccb282375787f98e0c6026c0183
karsazoltan <karsazoltan@sch.bme.hu>
2022-03-27 15:31:19 +0200

    celery env

--- sshjoker/celery.py
+++ sshjoker/celery.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from env import set_env
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sshjoker.settings')
+set_env()
 app = Celery('sshjoker')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
